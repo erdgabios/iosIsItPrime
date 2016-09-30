5bd2650802c11b7397c85d03ef5e0eac4bf360e4
gaberdios <gaberdios@gmail.com>
2016-09-30 19:34:51 +0200

    Done

--- Practise/ViewController.swift
+++ Practise/ViewController.swift
@@ -9,6 +9,52 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var enterNumber: UITextField!
+    
+    @IBAction func buttonTapped(_ sender: UIButton) {
+        
+        
+        if let enteredString = enterNumber.text {
+            let userEnteredInteger = Int(enteredString)
+            if let number = userEnteredInteger , number > 0 {
+                var isPrime = true
+                var i = 2
+                
+                
+                if number == 1 {
+                    isPrime = false
+                }
+                
+                
+                
+                while i < number {
+                    if number % i == 0 {
+                        isPrime = false
+                        
+                    }
+                    i+=1
+                }
+                if isPrime {
+                    result.text = "\(number): IS PRIME"
+                    result.textColor = UIColor.green
+                } else {
+                    result.text = "\(number): IS NOT PRIME"
+                    result.textColor = UIColor.red
+                }
+                
+            } else {
+                result.text = "GIVE ME A POSITIVE WHOLE NUMBER!"
+                result.textColor = UIColor.black
+                
+            }
+        }
+    }
+    
+    @IBOutlet weak var result: UILabel!
+    
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
